f604de92a82416991ed8846277c304dae7ccfb6e
aurelien <aurelien.richez@gmail.com>
2012-10-11 09:44:36 +0200

    last update

--- WebServiceDotNet/WebServiceDotNet/WebServiceDotNet/Contract.svc.cs
+++ WebServiceDotNet/WebServiceDotNet/WebServiceDotNet/Contract.svc.cs
@@ -15,21 +15,72 @@ namespace WebServiceDotNet
   
     public class Contract : IContract
     {
-        public const string COMPILER_PATH = "\"C:\\Program Files\\Java\\jdk1.6.0_31\\bin\\javac.exe\" ";
+        public const string JAVA_COMPILER_PATH = "\"C:\\Program Files\\Java\\jdk1.6.0_31\\bin\\javac.exe\" ";
+        public const string JAVA_EXECUTABLE_PATH = "\"C:\\Program Files\\Java\\jdk1.6.0_31\\bin\\java.exe\" ";
+        public const string JAVA_TECHNOLOGY = "JAVA";
+        public const string JAVA_BUILD_EXTENSION = ".java";
+        public const string JAVA_EXECUTION_EXTENSION = "";
+
+        public const string CSHARP_COMPILER_PATH = "\"C:\\Windows\\Microsoft.NET\\Framework\\v3.5\\csc.exe\"";
+        public const string CSHARP_TECHNOLOGY = "C#";
 
         public const string SRC_PATH = "D:\\";
 
-        public const string FILE_NAME = "Test.java";
+        public const string FILE_NAME = "Test";
 
         public const string POST_SOURCE_CODE = "sourceCode";
 
         public const string POST_FILENAME = "fileName";
 
+        public const string POST_TECHNOLOGY = "technology";
+
+        /// <summary>
+        /// Used to generate the random folder
+        /// </summary>
         private static Random _Generator = new Random(DateTime.Now.Millisecond);
 
-        public BuildResult Build(string filePath)
+        public Result BuildJavaFile(string filePath)
         {
-            BuildResult result = new BuildResult();
+            return Execute(JAVA_COMPILER_PATH, filePath);
+        }
+
+        public GlobalResult BuildFromSource(string technology, string src, string fileName = FILE_NAME)
+        {
+            int rdnFolder = _Generator.Next();
+            string folder = SRC_PATH + Path.DirectorySeparatorChar + rdnFolder;
+            Directory.CreateDirectory(folder);
+            string path = folder + Path.DirectorySeparatorChar + fileName;
+            StreamWriter fileWriter = new StreamWriter(path + JAVA_BUILD_EXTENSION);
+            fileWriter.Write(src);
+            fileWriter.Close();
+            Directory.SetCurrentDirectory(folder);
+            Result resultBuild = Execute(GetCompiler(technology), fileName + JAVA_BUILD_EXTENSION);
+            Result resultExecution = Execute(GetExecutable(technology), fileName + JAVA_EXECUTION_EXTENSION);
+            Directory.SetCurrentDirectory(Directory.GetParent(folder).FullName);
+            GlobalResult global = new GlobalResult();
+            global.Compilation = resultBuild;
+            global.Execution = resultExecution;
+            Directory.Delete(folder, true);
+            return global;
+            
+        }
+
+        public GlobalResult BuildFromSource(Stream src)
+        {
+            NameValueCollection postParameters = HttpUtility.ParseQueryString(new StreamReader(src).ReadToEnd());
+            string srcCode = postParameters.Get(POST_SOURCE_CODE);
+            string fileName = postParameters.Get(POST_FILENAME);
+            string technology = postParameters.Get(POST_TECHNOLOGY);
+
+            if (string.IsNullOrEmpty(fileName))
+                return BuildFromSource(technology, srcCode);
+            else
+                return BuildFromSource(technology, srcCode, fileName);
+        }
+
+        private static Result Execute(string executablePath, string filePath)
+        {
+            Result result = new Result();
             if (string.IsNullOrEmpty(filePath))
             {
                 result.Error = "ERROR";
@@ -37,46 +88,71 @@ namespace WebServiceDotNet
                 return result;
             }
             Process compiler = new Process();
-            ProcessStartInfo parameters = new ProcessStartInfo(COMPILER_PATH);//"cmd");
+            ProcessStartInfo parameters = new ProcessStartInfo(executablePath);
             parameters.Arguments = filePath;
             parameters.RedirectStandardOutput = true;
             parameters.RedirectStandardError = true;
+            
             //parameters.RedirectStandardInput = true;
             //parameters.CreateNoWindow = true;
+            
             parameters.UseShellExecute = false;
             compiler.StartInfo = parameters;
+            Stopwatch chronometer = new Stopwatch();
+            chronometer.Start();
             compiler.Start();
             compiler.WaitForExit();
-            result.Error = compiler.StandardError.ReadToEnd();
-            result.Output = compiler.StandardOutput.ReadToEnd();
+            chronometer.Stop();
+            result.TimeElapsed = chronometer.ElapsedMilliseconds / 1000;
+            result.Error = CleanResult(compiler.StandardError.ReadToEnd());
+            result.Output = CleanResult(compiler.StandardOutput.ReadToEnd());
             return result;
         }
 
-        public BuildResult BuildFromSource(string src, string fileName = FILE_NAME)
+        private static string CleanResult(string toClean)
         {
-            int rdnFolder = _Generator.Next();
-            string folder = SRC_PATH + Path.DirectorySeparatorChar + rdnFolder;
-            Directory.CreateDirectory(folder);
-            string path = folder + Path.DirectorySeparatorChar + fileName;
-            StreamWriter fileWriter = new StreamWriter(path);
-            fileWriter.Write(src);
-            fileWriter.Close();
-            BuildResult result = Build(path);
-            Directory.Delete(folder, true);
-            return result;
-            
+            string cleaned = toClean.Replace(Environment.NewLine, "<br/>");
+            return cleaned;
         }
 
-        public BuildResult BuildFromSourceStream(Stream src)
+        private static string GetCompiler(string technology)
         {
-            NameValueCollection postParameters = HttpUtility.ParseQueryString(new StreamReader(src).ReadToEnd());
-            string srcCode = postParameters.Get(POST_SOURCE_CODE);
-            string fileName = postParameters.Get(POST_FILENAME);
+            switch (technology.ToUpper().Trim())
+            {
+                case JAVA_TECHNOLOGY: return JAVA_COMPILER_PATH;
+                case CSHARP_TECHNOLOGY: return CSHARP_COMPILER_PATH;
+                default: return string.Empty;
+            }
+        }
 
-            if (string.IsNullOrEmpty(fileName))
-                return BuildFromSource(srcCode);
-            else
-                return BuildFromSource(srcCode, fileName);
+        private static string GetExecutable(string technology)
+        {
+            switch (technology.ToUpper().Trim())
+            {
+                case JAVA_TECHNOLOGY: return JAVA_EXECUTABLE_PATH;
+                case CSHARP_TECHNOLOGY: return string.Empty;
+                default: return string.Empty;
+            }
+        }
+
+        private static string GetBuildExtension(string technology)
+        {
+            switch (technology.ToUpper().Trim())
+            {
+                case JAVA_TECHNOLOGY: return JAVA_BUILD_EXTENSION;
+                case CSHARP_TECHNOLOGY: return string.Empty;
+                default: return string.Empty;
+            }
+        }
+
+        private static string GetExecutionExtension(string technology)
+        {
+            switch (technology.ToUpper().Trim())
+            {
+                case JAVA_TECHNOLOGY: return JAVA_EXECUTION_EXTENSION;
+                case CSHARP_TECHNOLOGY: return string.Empty;
+                default: return string.Empty;
+            }
         }
     }
 }
--- WebServiceDotNet/WebServiceDotNet/WebServiceDotNet/IContract.cs
+++ WebServiceDotNet/WebServiceDotNet/WebServiceDotNet/IContract.cs
@@ -13,25 +13,22 @@ namespace WebServiceDotNet
     public interface IContract
     {
         [OperationContract]
-        [WebGet(UriTemplate = "Build?filePath={filePath}")]
-        BuildResult Build(string filePath);
-
-        [OperationContract]
-        [WebInvoke(Method="POST", UriTemplate = "BuildFromSource?src={src}&fileName={fileName}")]
-        BuildResult BuildFromSource(string src, string fileName);
+        [WebGet(UriTemplate = "BuildJava?filePath={filePath}")]
+        Result BuildJavaFile(string filePath);
 
         [OperationContract]
         [WebInvoke(Method = "POST", UriTemplate = "BuildFromSourceStream")]
-        BuildResult BuildFromSourceStream(Stream src);
+        GlobalResult BuildFromSource(Stream src);
     }
 
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     [DataContract]
-    public class BuildResult
+    public class Result
     {
         private string output = string.Empty;
         private string error = string.Empty;
+        private long timeElapsed = 0;
 
         [DataMember]
         public string Error
@@ -46,5 +43,34 @@ namespace WebServiceDotNet
             get { return output; }
             set { output = value; }
         }
+
+        [DataMember]
+        public long TimeElapsed
+        {
+            get { return timeElapsed; }
+            set { timeElapsed = value; }
+        }
     }
+
+    [DataContract]
+    public class GlobalResult
+    {
+        private Result compilation = new Result();
+        private Result execution = new Result();
+
+        [DataMember]
+        public Result Compilation
+        {
+            get { return compilation; }
+            set { compilation = value; }
+        }
+
+        [DataMember]
+        public Result Execution
+        {
+            get { return execution; }
+            set { execution = value; }
+        }
+    }
+
 }
